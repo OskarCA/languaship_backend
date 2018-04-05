260c99afe163595f405f538b2feed91983e753b7
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-04-05 17:45:35 +0200

    add request spec for user

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -1,1 +1,20 @@
+RSpec.describe Api::V1::UserController, type: :request do
+  describe 'GET /api/v1/user' do
+    let!(:user) { create(:user) }
+    let(:object) { JSON.parse(response.body)}
 
+    it 'Should return all users' do
+      get '/api/v1/user', params: {
+        email: user.email, password: user.password
+      }, headers: headers
+
+      expected_response = {
+        'data' => {
+          'id' => user.id, 'uid' => user.email, 'email' => user.email,
+          'provider' => 'email', 'name' => nil, 'nickname' => nil,
+          'image' => nil
+        }
+      expect(object).to eq expected_response
+    end
+  end
+end
