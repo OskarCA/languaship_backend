affb728ccbb1c7409b80a556a0f27c9fa02bdfe0
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-04-06 19:24:25 +0200

    add user class to migration

--- db/migrate/20180405120530_devise_token_auth_create_users.rb
+++ db/migrate/20180405120530_devise_token_auth_create_users.rb
@@ -46,10 +46,17 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
+
     add_index :users, :email,                unique: true
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
+  end
+
+  class User < ApplicationRecord
+    def tokens_has_json_column_type?
+      false
+    end
   end
 end
