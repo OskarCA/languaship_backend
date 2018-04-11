c88c74320295bd6b50c18eaf4ebd0e5e69ac23f4
Oskar Brännström <oskar@quad.se>
2018-04-11 13:54:40 +0200

    Add assosiations between location and user

--- db/migrate/20180411092111_create_locations.rb
+++ db/migrate/20180411092111_create_locations.rb
@@ -2,8 +2,6 @@ class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
       t.string :locale
-      
-
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,40 @@
-ActiveRecord::Schema.define(version: 2018_04_05_120530) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_04_11_092111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "languages", force: :cascade do |t|
+    t.boolean "native"
+    t.boolean "learn"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "languages_users", id: false, force: :cascade do |t|
+    t.bigint "language_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["language_id", "user_id"], name: "index_languages_users_on_language_id_and_user_id"
+    t.index ["user_id", "language_id"], name: "index_languages_users_on_user_id_and_language_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "locale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -32,4 +65,5 @@ ActiveRecord::Schema.define(version: 2018_04_05_120530) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
+
 end
