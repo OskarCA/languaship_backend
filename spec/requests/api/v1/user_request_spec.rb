7b8a09cf6dcfa21127e3eb31b3fbe58df8979437
Oskar Brännström <oskar@quad.se>
2018-04-11 11:04:33 +0200

    Fixed the error message by placing code in describe blocks

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -7,25 +7,28 @@ RSpec.describe Api::V1::UserController, type: :request do
     let(:credentials) { user.create_new_auth_token }
     let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
 
-    context 'create two users' do
-      before do
-        2.times do
-          create(:user)
-        end
+    describe 'oneUser' do 
+      it 'Should return one user' do
+        get "/api/v1/user/#{user.id}", headers: headers
+  
+        expected_response = eval(file_fixture('user.txt').read)
+        expect(object).to eq expected_response
       end
     end
-
-    it 'Should return all users' do
-      get '/api/v1/user', headers: headers
-      expected_response = eval(file_fixture('users.txt').read)
-      expect(object).to eq expected_response
-    end
-
-    it 'Should return one user' do
-      get "/api/v1/user/#{user.id}", headers: headers
-
-      expected_response = eval(file_fixture('user.txt').read)
-      expect(object).to eq expected_response
+    
+    describe 'multibleUsers' do 
+      context 'create two users' do
+        before do
+          2.times do
+            create(:user)
+          end
+        end
+        it 'Should return all users' do
+          get '/api/v1/user', headers: headers
+          expected_response = eval(file_fixture('users.txt').read)
+          expect(object).to eq expected_response
+        end  
+      end
     end
   end
 end
