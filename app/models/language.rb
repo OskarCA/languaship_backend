072621f7c4e3c8448f7142ad884f08f5001a9c22
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-04-12 13:46:32 +0200

    uncomment authorization on user controller

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,5 @@
 class Language < ApplicationRecord
   has_and_belongs_to_many :users
-  # validates :name, presence: true
+
   validates_presence_of :name
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 User.create(email: 'fake@fak.com', password: 'password', name: 'hello', id: 4, )
+Language.create(name: 'English', native: true, learn: false)
+Location.create(locale: 'Stockholm', user_id: 4)
